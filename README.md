5a309afb2dfc547a189906b004f7ca48ff13ffac
MikaAK <mikakalathil@gmail.com>
2016-03-06 17:58:08 -0800

    start on devobx script

---
+++ README.md
@@ -0,0 +1,4 @@
+DevBox
+====
+
+Script to create a dev box with n for management
---
+++ run.sh
@@ -0,0 +1,3 @@
+curl -sL get.fisherman.sh | fish & curl -L http://git.io/n-install | bash && wait &&
+. ~/.bashrc &&
+fisher install available
