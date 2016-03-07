165a2dce34ee8b8d9e4b0258f4cf0cfd214f0da6
Mika Kalathil <me@mikakalathil.ca>
2016-03-07 00:53:37 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ xcode-select –-install &&
 curl -sL get.fisherman.sh | bash && curl -L http://git.io/n-install | bash &&
 . ~/.bashrc && fisher install available &&
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
-brew doctor &&
+brew doctor ;
 brew install caskroom/cask/brew-cask && 
-brew cask install Chrome && brew install imagemagick postgresql && wait
+brew cask install google-chrome && brew install imagemagick postgresql elixir && 
+ssh-keygen -t rsa
