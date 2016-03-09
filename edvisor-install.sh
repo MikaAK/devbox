214916c66c3aa44294af4e047bdf0e0c04bef8b2
Mika Kalathil <me@mikakalathil.ca>
2016-03-09 02:08:30 -0800

    Update edvisor-install.sh

--- edvisor-install.sh
+++ edvisor-install.sh
@@ -4,4 +4,7 @@ mysql.server start &&
 mysql -u root -e 'create database edvisor_dev;' &&
 cd ~/Documents &&
 mkdir Edvisor && cd Edvisor &&
-git clone -p edvisor-io/jsedvisor app
+git clone -p edvisor-io/jsedvisor app &&
+cd app &&
+npm i && npm link && 
+cd client && npm i
