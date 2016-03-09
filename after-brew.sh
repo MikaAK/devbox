a56f41b6c64f0186285e6a4ec67fb3eee10352a3
Mika Kalathil <me@mikakalathil.ca>
2016-03-09 01:43:23 -0800

    Update after-brew.sh

--- after-brew.sh
+++ after-brew.sh
@@ -1,5 +1,6 @@
 brew install caskroom/cask/brew-cask && 
-brew cask install google-chrome && brew install git hub fish imagemagick postgresql elixir cmake && 
-ssh-keygen -t rsa &&
-. ~/.bash_profile && 
+brew cask install google-chrome &&
+brew install git hub fish imagemagick postgresql elixir cmake &&
+
+. ~/.bash_profile &&
 curl -sL get.fisherman.sh | fish
