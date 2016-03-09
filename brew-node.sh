8bbefe0a0c04223ba87e01fec2a91b17f88c0493
Mika Kalathil <me@mikakalathil.ca>
2016-03-09 00:26:55 -0800

    Update and rename run.sh to brew-node.sh

---
+++ brew-node.sh
@@ -0,0 +1,3 @@
+curl -L http://git.io/n-install | bash &&
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
+brew doctor
--- run.sh
+++
@@ -1,8 +0,0 @@
-curl -L http://git.io/n-install | bash &&
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
-brew doctor ;
-brew install caskroom/cask/brew-cask && 
-brew cask install google-chrome && brew install imagemagick postgresql elixir && 
-ssh-keygen -t rsa &&
-. ~/.bashrc && 
-curl -sL get.fisherman.sh | fish ; fisher install available
