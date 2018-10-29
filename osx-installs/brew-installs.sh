f7eacd3481c34ad7cdee2335d1dccccf23447892
Mika Kalathil <mika@kalathil.me>
2018-10-29 15:00:42 -0700

    Update brew-installs.sh

--- osx-installs/brew-installs.sh
+++ osx-installs/brew-installs.sh
@@ -2,9 +2,10 @@
 
 brew install -y git hub fish postgresql coreutils automake autoconf \
                 openssl libyaml readline libxslt libtool unixodbc \
-                gpg rust python3 neovim awscli
+                gpg rust python3 neovim awscli pinentry-mac
 
 sudo bash -c "echo '/usr/local/bin/fish' >> /etc/shells" &&
 sudo chsh -s /usr/local/bin/fish &&
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher &&
-brew cleanup
+echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf && 
+killall gpg-agent & brew cleanup
