65b3c5893002abed40ad590b51a9acd61261e18b
Mika Kalathil <mika@kalathil.me>
2022-02-28 20:26:52 -0700

    Update setup.sh

--- git-aliases/setup.sh
+++ git-aliases/setup.sh
@@ -1,4 +1,15 @@
+git config --global user.name 'MikaAK'
+git config --global user.email 'mika@kalathil.me'
 git config --global alias.ncommit 'commit --no-verify --no-edit'
 git config --global alias.npush 'push --no-verify'
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.ignore-file 'update-index --assume-unchanged'
+git config --global init.defaultBranch main
+git config --global pull.rebase true
+git config --global pull.ff only
+git config --global hub.protocol ssh 
+git config --global gpg.program gpg
+git config --global commit.gpgsign true
+git config --global user.signingkey 4A1B3D37AB09E6FC
+git config --global alias.ignore-file 'update-index --assume-unchanged'
+git config --global alias.unignore-file 'update-index --no-assume-unchanged'
