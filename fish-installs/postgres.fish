38117a1bdd4114433dd4bf108d855dd789d8870b
Mika Kalathil <mika@kalathil.me>
2022-03-01 11:27:38 -0700

    Update postgres.fish

--- fish-installs/postgres.fish
+++ fish-installs/postgres.fish
@@ -1,7 +1,8 @@
 #! /usr/bin/env fish
 
 set -Ux PGDATA /opt/homebrew/var/postgres;
-and pg_ctl -l $PGDATA/server.log start;
-and createuser -s postgres;
-and createuser -s (whoami);
-and createdb (whoami);
+and pg_ctl -l $PGDATA/server.log start
+
+createuser -s postgres
+createuser -s (whoami)
+createdb (whoami)
