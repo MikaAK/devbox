da5fde30fe3bba4a38086ca68473a6f3b9f69eb9
Mika Kalathil <mika@kalathil.me>
2022-02-28 20:34:16 -0700

    Update brew-installs.sh

--- osx-installs/brew-installs.sh
+++ osx-installs/brew-installs.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-brew install -y git gh fish postgresql coreutils automake autoconf \
+brew install    git gh fish postgresql coreutils automake autoconf \
                 openssl libyaml readline libxslt libtool unixodbc \
-                gpg rust python3 neovim awscli pinentry-mac
+                gpg rust python3 neovim awscli pinentry-mac gcc zlib curl ossp-uuid
 
 sudo bash -c "echo '/opt/homebrew/bin/fish' >> /etc/shells" &&
 chsh -s /opt/homebrew/bin/fish &&
