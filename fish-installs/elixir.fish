9d576a0b35b0966d54464d4673666ad4eca51a82
Mika Kalathil <mika@kalathil.me>
2022-02-28 19:34:44 -0700

    Update elixir.fish

--- fish-installs/elixir.fish
+++ fish-installs/elixir.fish
@@ -1,9 +1,9 @@
 #! /usr/bin/env fish
 
-set erlang_version 21.0.6
-set elixir_version 1.7.3
+set erlang_version 24.1.2
+set elixir_version 1.12.3-otp-24
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.1
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
 and echo 'source ~/.asdf/asdf.fish' >> ~/.config/fish/config.fish
 and source ~/.asdf/asdf.fish 
 and mkdir -p ~/.config/fish/completions 
