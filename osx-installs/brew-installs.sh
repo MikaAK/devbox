24a35d393a05b214a60fa0b7866ae9a375c30919
Mika Kalathil <mika@kalathil.me>
2022-02-28 19:18:57 -0700

    Update brew-installs.sh

--- osx-installs/brew-installs.sh
+++ osx-installs/brew-installs.sh
@@ -4,8 +4,8 @@ brew install -y git gh fish postgresql coreutils automake autoconf \
                 openssl libyaml readline libxslt libtool unixodbc \
                 gpg rust python3 neovim awscli pinentry-mac
 
-sudo bash -c "echo '/usr/local/bin/fish' >> /etc/shells" &&
-sudo chsh -s /usr/local/bin/fish &&
+sudo bash -c "echo '/opt/homebrew/bin/fish' >> /etc/shells" &&
+sudo chsh -s /opt/homebrew/bin/fish &&
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher &&
-echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf && 
+echo "pinentry-program /opt/homebrew/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf && 
 killall gpg-agent & brew cleanup
