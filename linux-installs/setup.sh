bf4d561db11fee3e22f1c32b6811f8041f85bfe3
Mika Kalathil <mika@kalathil.me>
2022-07-30 12:12:33 -0700

    Update setup.sh

--- linux-installs/setup.sh
+++ linux-installs/setup.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-sudo apt-add-repository ppa:fish-shell/release-2 -y &&
+sudo apt-add-repository ppa:fish-shell/release-3 -y &&
 sudo apt-get update -y &&
 sudo apt-get install -y python3 python3-pip curl wget vim neovim automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev git fish postgresql xsltproc fop cmake ffmpeg &&
 pip3 install neovim &&
