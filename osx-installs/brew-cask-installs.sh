bb71ac48b4603a62643aa41060290ebdab0c6327
Mika Kalathil <mika@kalathil.me>
2024-12-02 20:50:28 -0800

    Update brew-cask-installs.sh

--- osx-installs/brew-cask-installs.sh
+++ osx-installs/brew-cask-installs.sh
@@ -2,4 +2,4 @@
 
 brew install --cask alfred dash google-drive slack \
                     bartender google-chrome iterm2 \
-                    sizeup obsidian
+                    sizeup obsidian datagrip
