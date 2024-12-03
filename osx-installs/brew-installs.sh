91cc169c2d4e4f54d5d22f2102ab9fa87db8e516
MikaAK <mika@kalathil.me>
2024-12-02 17:45:37 -0800

    update

--- osx-installs/brew-installs.sh
+++ osx-installs/brew-installs.sh
@@ -3,10 +3,10 @@
 brew install    git gh fish postgresql coreutils automake autoconf \
                 openssl libyaml readline libxslt libtool unixodbc wxwidgets autoconf \
                 gpg rust python3 neovim awscli pinentry-mac gcc zlib curl ossp-uuid \
-                bat eza ripgrep fd sd procs
+                bat eza ripgrep fzf fd sd procs
 
 sudo bash -c "echo '/opt/homebrew/bin/fish' >> /etc/shells" &&
 chsh -s /opt/homebrew/bin/fish &&
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher &&
-echo "pinentry-program /opt/homebrew/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf && 
+echo "pinentry-program /opt/homebrew/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf &&
 killall gpg-agent & brew cleanup
