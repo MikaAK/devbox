38bbcb0bcf6f943b05ca798224778c83bfc4bb9c
Mika Kalathil <mika@kalathil.me>
2024-12-03 16:40:20 -0800

    Update postgres.fish

--- fish-installs/postgres.fish
+++ fish-installs/postgres.fish
@@ -1,6 +1,6 @@
 #! /usr/bin/env fish
 
-set -Ux PGDATA /opt/homebrew/var/postgres;
+set -Ux PGDATA /opt/homebrew/var/(ls /opt/homebrew/var/ | grep postgres | tail -n 1)
 and pg_ctl -l $PGDATA/server.log start
 
 createuser -s postgres
