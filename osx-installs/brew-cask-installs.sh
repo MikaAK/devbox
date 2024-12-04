a7621a1f24c09ed7ec4c213a9326ac2f0208ef68
Mika Kalathil <mika@kalathil.me>
2024-12-03 17:01:35 -0800

    Update brew-cask-installs.sh

--- osx-installs/brew-cask-installs.sh
+++ osx-installs/brew-cask-installs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 brew install --cask alfred dash google-drive slack \
-                    bartender google-chrome iterm2 vimr \
+                    bartender google-chrome iterm2 vimr discord \
                     sizeup obsidian datagrip motion 1password beeper
