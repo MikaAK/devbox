b860a7385e8812fe1b3717e479b8da73597d3f91
Mika Kalathil <mika@kalathil.me>
2025-02-07 23:10:17 -0800

    Update brew-cask-installs.sh

--- osx-installs/brew-cask-installs.sh
+++ osx-installs/brew-cask-installs.sh
@@ -2,4 +2,5 @@
 
 brew install --cask alfred dash google-drive slack livebook cursor \
                     bartender google-chrome iterm2 vimr discord \
-                    sizeup obsidian datagrip motion 1password beeper 
+                    sizeup obsidian datagrip motion 1password beeper \
+                    sonos permute descript krisp
